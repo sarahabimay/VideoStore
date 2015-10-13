f7a773ea03104e80342be01ec532f65fe001d2f2
sarahabimay <sarahabimay@gmail.com>
2015-10-13 14:49:42 +0100

    Make Movie attributes in Test class

--- src/CustomerTest.java
+++ src/CustomerTest.java
@@ -6,50 +6,66 @@ import static org.junit.Assert.*;
 public class CustomerTest {
 
     private Customer customer;
+    private Movie newRelease1;
+    private Movie newRelease2;
+    private Movie childrens1;
+    private Movie childrens2;
+    private Movie childrens3;
+    private Movie regular1;
+    private Movie regular2;
+    private Movie regular3;
 
     @Before
     public void setUp() throws Exception {
         customer = new Customer("Fred");
+        newRelease1 = new NewReleaseMovie("The Cell");
+        newRelease2 = new NewReleaseMovie("The Tigger Movie");
+        childrens1 = new ChildrensMovie("The Tigger Movie");
+        childrens2 = new ChildrensMovie("Cinderella");
+        childrens3 = new ChildrensMovie("Bambi");
+        regular1 = new RegularMovie("Plan 9 from Outer Space");
+        regular2 = new RegularMovie("8 1/2");
+        regular3 = new RegularMovie("Eraserhead");
     }
 
     @Test
     public void testSingleNewReleaseStatement() {
-        customer.addRental(new Rental(new NewReleaseMovie("The Cell"), 3));
+        customer.addRental(new Rental(newRelease1, 3));
         assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement());
     }
 
     @Test
     public void testDualNewReleaseStatement() {
-        customer.addRental(new Rental(new NewReleaseMovie("The Cell"), 3));
-        customer.addRental(new Rental(new NewReleaseMovie("The Tigger Movie"), 3));
+        customer.addRental(new Rental(newRelease1, 3));
+        customer.addRental(new Rental(newRelease2, 3));
         assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement());
     }
 
     @Test
     public void testSingleChildrensStatement() {
-        customer.addRental(new Rental(new ChildrensMovie("The Tigger Movie"), 3));
+        customer.addRental(new Rental(childrens1, 3));
         assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement());
     }
 
     @Test
     public void testDualChildrensStatement() {
-        customer.addRental(new Rental(new ChildrensMovie("The Tigger Movie"), 3));
-        customer.addRental(new Rental(new ChildrensMovie("Cinderella"), 5));
+        customer.addRental(new Rental(childrens1,3));
+        customer.addRental(new Rental(childrens2, 5));
         assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\n\tCinderella\t4.5\nYou owed 6.0\nYou earned 2 frequent renter points\n", customer.statement());
     }
 
     @Test
     public void testMultipleChildrensStatement() {
-        customer.addRental(new Rental(new ChildrensMovie("The Tigger Movie"), 3));
-        customer.addRental(new Rental(new ChildrensMovie("Cinderella"), 5));
-        customer.addRental(new Rental(new ChildrensMovie("Bambi"), 1));
+        customer.addRental(new Rental(childrens1, 3));
+        customer.addRental(new Rental(childrens2, 5));
+        customer.addRental(new Rental(childrens3, 1));
         assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\n\tCinderella\t4.5\n\tBambi\t1.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.statement());
 
     }
 
     @Test
     public void childrensWithMoreThanThreeDaysRentalStatement() {
-        customer.addRental(new Rental(new ChildrensMovie("The Tigger Movie"), 4));
+        customer.addRental(new Rental(childrens1, 4));
         assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t3.0\nYou owed 3.0\nYou earned 1 frequent renter points\n", customer.statement());
     }
 
