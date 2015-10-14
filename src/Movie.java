0dc839ac2f3b693de8a0b9178fc2eec72c72c425
sarahabimay <sarahabimay@gmail.com>
2015-10-14 12:02:30 +0100

    Re-clone from VideoStore repo
    Re-do the VideoStore exercise after watching Uncle Bob's video.

--- src/Movie.java
+++ src/Movie.java
@@ -1,15 +1,27 @@
-public abstract class Movie {
+public class Movie
+{
+	public static final int CHILDRENS	= 2;
+	public static final int REGULAR 	= 0;
+	public static final int NEW_RELEASE = 1;
 
-    private String title;
+	private String title;
+	private int priceCode;
 
-    public Movie(String title) {
-        this.title = title;
-    }
+	public Movie (String title, int priceCode) {
+		this.title 		= title;
+		this.priceCode 	= priceCode;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public int getPriceCode () {
+		return priceCode;
+	}
 
-    public abstract double rentalAmount(int daysRented);
-    public abstract int frequentRenterPoints(int daysRented);
+	public void setPriceCode (int code) {
+		priceCode = code;
+	}
+
+	public String getTitle () {
+		return title;
+	}
+
 }
--- src/Rental.java
+++ src/Rental.java
@@ -1,25 +1,18 @@
-public class Rental {
-    private Movie movie;
-    private int daysRented;
-    private double rentalAmount;
-    private int frequentRenterPoints;
+public class Rental
+{
+	public Rental (Movie movie, int daysRented) {
+		this.movie 		= movie;
+		this.daysRented = daysRented;
+	}
 
-    public Rental(Movie movie, int daysRented) {
-        this.movie = movie;
-        this.daysRented = daysRented;
-        this.rentalAmount = movie.rentalAmount(daysRented);
-        this.frequentRenterPoints = movie.frequentRenterPoints(daysRented);
-    }
+	public int getDaysRented () {
+		return daysRented;
+	}
 
-    public double getRentalAmount() {
-        return rentalAmount;
-    }
+	public Movie getMovie () {
+		return movie;
+	}
 
-    public int getFrequentRenterPoints() {
-        return frequentRenterPoints;
-    }
-
-    public String getMovieTitle() {
-        return movie.getTitle();
-    }
+	private Movie movie;
+	private int daysRented;
 }
